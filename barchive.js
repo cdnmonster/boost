c2c531c9d27ee43ddb782da41df94fd7b807a78c
cdnmonster <cdnmonster@users.noreply.github.com>
2015-07-23 01:39:03 +0530

    Update barchive.js

--- barchive.js
+++ barchive.js
@@ -2,10 +2,7 @@ function LoadTheArchive(TotalFeed)
 {
     var PostTitles = new Array();
     var PostURLs = new Array();
-    var PostYears = new Array();
-    var PostMonths = new Array();
-    var PostDays = new Array();
-    if("entry" in TotalFeed.feed) 
+       if("entry" in TotalFeed.feed) 
     {
 	var PostEntries=TotalFeed.feed.entry.length;
 	for(var PostNum=0; PostNum<PostEntries ; PostNum++) 
@@ -32,8 +29,11 @@ function LoadTheArchive(TotalFeed)
 
 function DisplaytheTOC(PostTitles,PostURLs,PostYears,PostMonths,PostDays)
 {
-    
+    var MonthNames=["January","February","March","April","May","June","July","August","September","October","November","December"];
     var NumberOfEntries=PostTitles.length;
     for(var EntryNum = 0; EntryNum < NumberOfEntries; EntryNum++)
-    
+    {
+	NameOfMonth = MonthNames[parseInt(PostMonths[EntryNum],10)-1]
+	document.write('<a href ="'+PostURLs[EntryNum]+'">'+PostTitles[EntryNum]+"</a> ("+NameOfMonth+" "+parseInt(PostDays[EntryNum],10)+", "+PostYears[EntryNum]+")<br />");
+    }
 }
